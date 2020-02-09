6601e39bcd34ffef1242826c4a8ddc1a106a58c7
EC2 Default User <ec2-user@ip-172-31-33-247.ap-northeast-1.compute.internal>
2020-02-09 03:29:28 +0000

    advance-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,30 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember #save to DB
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget  #remember_digestをnil
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
@@ -14,6 +35,7 @@ module SessionsHelper
   end
   
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
