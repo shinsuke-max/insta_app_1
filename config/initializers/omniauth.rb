31b51c1061175bbb9a1fd53c9431c3db73b968af
EC2 Default User <ec2-user@ip-172-31-33-247.ap-northeast-1.compute.internal>
2020-02-15 05:46:05 +0000

    facebook-login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['182825779607221'], ENV['c3ee70733014685c038e230b85a80400'],
-  scope: 'email', display: 'popup', local: 'ja_JP', info_fields: "id, name, gender"
+  provider :facebook, ENV['182825779607221'], ENV['283f280451c589b92efe99c598f8e9cf']
 end
