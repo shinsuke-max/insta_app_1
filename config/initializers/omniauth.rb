7fa95131d08d209d43c4e366f93199e4625610ef
EC2 Default User <ec2-user@ip-172-31-33-247.ap-northeast-1.compute.internal>
2020-02-20 12:29:59 +0000

    hide env-key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['182825779607221'], ENV['283f280451c589b92efe99c598f8e9cf']
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET_KEY']
 end
