0546d2f670858a0864e0493a88909e39a3590d98
shinsuke <shinst710@gmail.com>
2020-03-10 01:08:36 +0900

    updating-rails-version

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module InstaApp
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.action_view.embed_authenticity_token_in_remote_forms = true
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
